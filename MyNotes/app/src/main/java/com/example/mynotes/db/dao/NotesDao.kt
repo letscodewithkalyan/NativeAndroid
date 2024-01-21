a3ed1ede2f8097a17186d9869e5f1b5a2257b4ce
Kalyan Pidugu <pidugukalyan@gmail.com>
2024-01-21 17:00:39 +0530

    Added DB

---
+++ MyNotes/app/src/main/java/com/example/mynotes/db/dao/NotesDao.kt
@@ -0,0 +1,27 @@
+package com.example.mynotes.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.mynotes.db.entities.Notes
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun Insert(notes: Notes)
+
+    @Update
+    suspend fun update(notes: Notes)
+
+    @Delete
+    suspend fun delete(notes: Notes)
+
+    @Query("SELECT * FROM notes_table")
+    suspend fun getAllNotes(): List<Notes>
+
+    @Query("SELECT * FROM notes_table where id = :id")
+    suspend fun getNoteById(id: Int)
+}
